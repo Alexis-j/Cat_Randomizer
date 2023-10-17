816124cb9955a8cb07a5e7c9c5e4f8e572000632
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-10-17 16:15:25 +0200

    endpoint fact

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ APIs:
 - Facts Random: https://catfatc.ninja/fact
 - ImageRandom: https://catas.com/cat/says/hello
 
+- endpoint para usar: `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
 
 - recupera una echo aleatorio de gatos de la primera API.
 - mostrar la primera letra de echo
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import { useEffect, useState } from "react"
 
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+// const CAT_ENDPOINT_IMG_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/'
 export function App () {
-  const [fact, setFact] = useState('lorem ipsum cat act whatever')
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
-    fetch('https://catfatc.ninja/fact')
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
-      .then(data => setFact(data.fact))
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const firstWord = fact.split(' ', 3).join('')
+
+        fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+          .then(res => res.json())
+          .then(response => {
+            const { url } = response
+            setImageUrl(url)
+          })
+      })
   }, [])
   return (
     <main>
       <h1>App de Gatitos</h1>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words from ${fact}`} />}
     </main>
   )
 }
