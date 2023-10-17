4a9c66c427ed222b586e8bbb20483218c5e33d73
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-10-17 15:23:30 +0200

    endpoint fact

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ APIs:
 - ImageRandom: https://catas.com/cat/says/hello
 
 
-recupera una echo aleatorio de gatos de la primera API y muestra una imagen de gato de un gato con la primera palabra del echo recuperado
+- recupera una echo aleatorio de gatos de la primera API.
+- mostrar la primera letra de echo
+- muestra una imagen de gato de un gato con la primera palabra del echo recuperado
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-export function App() {
+export function App () {
+  const [fact, setFact] = useState('lorem ipsum cat act whatever')
 
-  
-  return(
-    <h1>App de Gatitos</h1>
+  useEffect(() => {
+    fetch('https://catfatc.ninja/fact')
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  }, [])
+  return (
+    <main>
+      <h1>App de Gatitos</h1>
+      <p>{fact}</p>
+    </main>
   )
 }
