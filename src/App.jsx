f8279ae1bfe3f215ac82459c60fff5ffca6ab058
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-10-19 10:59:06 +0200

    terminando l aapp y areglando pequenos erroe

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,46 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 import './app.css'
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
-const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/cat/says/';
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/cat/says/'
 
-export function App() {
-  const [fact, setFact] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
+export function App () {
+  const [fact, setFact] = useState('')
+  const [imageUrl, setImageUrl] = useState('')
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
       .then((data) => {
-        const { fact } = data;
-        setFact(fact);
+        const { fact } = data
+        setFact(fact)
 
-        const firstthreeWords = fact.split(' ').slice(0, 3).join(' ');
+        const firstthreeWords = fact.split(' ').slice(0, 3).join(' ')
 
-        fetch(`https://cataas.com/cat/says/${firstthreeWords}?size=50&color=red&json=true`)
+        fetch(`${CAT_PREFIX_IMAGE_URL}${firstthreeWords}?size=50&color=red&json=true`)
           .then((res) => res.json())
           .then((response) => {
-            const { _id } = firstthreeWords;
+            const { _id } = firstthreeWords
             // Construir la URL de la imagen correctamente
-            const imageUrl = `https://cataas.com/cat/says/${firstthreeWords }`;
+            const imageUrl = `https://cataas.com/cat/says/${_id}`
 
-            setImageUrl(imageUrl);
+            setImageUrl(imageUrl)
           })
-      });
-  }, []);
+      })
+  }, [])
 
   return (
     <main>
-      <div className="card">
-        <h1 class="title">App de Gatitos</h1>
-        <section>
-          <div className="echo">
-          {fact && <p>{fact}</p>}
-          </div>
-          {imageUrl && (
-            <img
-              src={imageUrl} // Utiliza la URL de la imagen directamente aquí
-              alt={`Image extracted using the first three words from ${fact}`}
-            />
-          )}
-        </section>
-      </div>
+      <h1>App de Gatitos</h1>
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && (
+          <img
+            src={imageUrl} // Utiliza la URL de la imagen directamente aquí
+            alt={`Image extracted using the first three words from ${fact}`}
+          />
+        )}
+      </section>
     </main>
-  );
+  )
 }
